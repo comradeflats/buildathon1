4cd98fb5d1b4f3f5606c0e39d780371bd2ad1963
comradeflats <ryanwinstonelliott@gmail.com>
2026-03-08 14:16:53 +0700

    chore: update basePath for buildathon1 repository

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // This must match your repository name
-  basePath: '/buildathon',
-  assetPrefix: '/buildathon',
+  // This must match your NEW repository name
+  basePath: '/buildathon1',
+  assetPrefix: '/buildathon1',
 };
 
 module.exports = nextConfig;
